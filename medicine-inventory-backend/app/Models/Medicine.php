2f8e58a337aa2440822eb55af39692c727e90797
shim-zzz <shielaamaee@gmail.com>
2026-09-16 09:46:31 +0800

    Added medicine model & table

--- medicine-inventory-backend/app/Models/Medicine.php
+++ medicine-inventory-backend/app/Models/Medicine.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Medicine extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'category',
+        'quantity',
+    ];
 }
--- medicine-inventory-backend/database/migrations/2026_09_16_013113_create_medicines_table.php
+++ medicine-inventory-backend/database/migrations/2026_09_16_013113_create_medicines_table.php
@@ -12,9 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('medicines', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+         $table->id();
+         $table->string('name');
+         $table->string('category');
+         $table->integer('quantity');
+         $table->decimal('price', 10, 2);
+         $table->timestamps();
+     });
     }
 
     /**
